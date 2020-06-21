bf182927a7335ea1a9648c5e2a66a888bb5e6542
imabritishcow <m@bcow.tk>
2020-06-21 17:40:39 -0400

    Now uses BOT_ADMINS

--- commands/setn.js
+++ commands/setn.js
@@ -9,12 +9,10 @@ module.exports = {
 	description: 'Sets nation information',
 	execute: (message, args, Nation) => {
 		let errorMessage = new Discord.MessageEmbed().setTitle(':x: **Error**').setColor(0xdc2e44).setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png');
-		let successMessage = new Discord.MessageEmbed().setTitle(':white_check_mark: **Success!**').setColor(0x07bf63).setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png');
-		if (message.author.id != '456965312886079533') {
-			if (message.author.id != '345720683076124673') {
-				message.channel.send(errorMessage.setDescription('You do not have permission to use this command.'));
-			}
-		}
+    let successMessage = new Discord.MessageEmbed().setTitle(':white_check_mark: **Success!**').setColor(0x07bf63).setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png');
+    
+    if(!config.BOT_ADMINS.includes(message.author.id)) return message.channel.send(errorMessage.setDescription("You do not have permission to use this command."));
+    
 		if (!args[2]) return message.channel.send(errorMessage.setDescription('Missing username or UUID. Command usage: 1!setn [type] [nation] <- Missing [value]'));
 		if (args[1] == 'delete') {
 		} else {
