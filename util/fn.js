516292e4242056f5579c7a136622bfd6ca7ff850
imabritishcow <m@bcow.tk>
2020-06-29 11:31:35 -0400

    Improved paginator

--- util/fn.js
+++ util/fn.js
@@ -5,27 +5,23 @@ let paginator = async (author, msg, embeds, pageNow, addReactions = true) => {
     await msg.react("▶")
     await msg.react("⏩")
   }
-  let reaction = await msg.awaitReactions((reaction, user) => user.id == author && ["◀","▶","⏪","⏩"].includes(reaction.emoji.name), {time: 90*1000, max:1, errors: ['time']}).catch(() => {})
+  let reaction = await msg.awaitReactions((reaction, user) => user.id == author && ["◀","▶","⏪","⏩"].includes(reaction.emoji.name), {time: 240*1000, max:1, errors: ['time']}).catch(() => {})
   if (!reaction) return msg.reactions.removeAll().catch(() => {})
   reaction = reaction.first()
   
   if (msg.channel.type == 'dm' || !msg.member.hasPermission("MANAGE_MESSAGES")) {
     if (reaction.emoji.name == "◀") {
-      let m = await msg.channel.send(embeds[Math.max(pageNow-1, 0)])
-      msg.delete()
-      paginator(author, m, embeds, Math.max(pageNow-1, 0))
+      let m = await msg.edit(embeds[Math.max(pageNow-1, 0)])
+      paginator(author, m, embeds, Math.max(pageNow-1, 0), false)
     } else if (reaction.emoji.name == "▶") {
-      let m = await msg.channel.send(embeds[Math.min(pageNow+1, embeds.length-1)])
-      msg.delete()
-      paginator(author, m, embeds, Math.min(pageNow+1, embeds.length-1))
+      let m = await msg.edit(embeds[Math.min(pageNow+1, embeds.length-1)])
+      paginator(author, m, embeds, Math.min(pageNow+1, embeds.length-1), false)
     } else if (reaction.emoji.name == "⏪") {
-      let m = await msg.channel.send(embeds[0])
-      msg.delete()
-      paginator(author, m, embeds, 0)
+      let m = await msg.edit(embeds[0])
+      paginator(author, m, embeds, 0, false)
     } else if (reaction.emoji.name == "⏩") {
-      let m = await msg.channel.send(embeds[embeds.length-1])
-      msg.delete()
-      paginator(author, m, embeds, embeds.length-1)
+      let m = await msg.edit(embeds[embeds.length-1])
+      paginator(author, m, embeds, embeds.length-1, false)
     }
   } else {
     if (reaction.emoji.name == "◀") {
