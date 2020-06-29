3f057d0a8fc87472b41e825292a0cc62a72b0dba
imabritishcow <m@bcow.tk>
2020-06-28 21:59:08 -0400

    Removed old commands

--- commands/help.js
+++ commands/help.js
@@ -16,11 +16,6 @@ module.exports = {
 			.addField('1!t [town]', 'Finds town information')
 			.addField('1!notown', 'Gets towns without a nation')
 			.addField('1!pl or player [username or UUID]', 'Gets player information')
-			.addField('1!getall', 'Sends all results in the database to your DMs in pages')
-			.addField('**Special Search Results**', '‍')
-			.addField('1!calculator [num1] [operator] [num2]', 'Supports addition, subtraction, multiplication, exponents, and division')
-			.addField('1!coinflip', 'Flips a coin')
-			.addField('1!randomnumber [min] [max]', 'Generates a random number')
 			.setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png');
 		message.channel.send(helpEmbed);
 	}
