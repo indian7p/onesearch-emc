6895e766904d6c91308dc200216eccadd9471abb
imabritishcow <m@bcow.tk>
2020-06-29 22:05:13 -0400

    Added residents and town count

--- commands/nonation.js
+++ commands/nonation.js
@@ -29,31 +29,37 @@ module.exports = {
 		Town.find({ nation: 'No Nation' }).sort(sortingOpts).exec(async function (err, towns) {
 			if (err) return message.channel.send(errorMessage.setDescription('An error occurred.'));
 			if (!towns) return message.channel.send(errorMessage.setDescription('The database may be updating. Try again later.'));
+			Nation.findOne({ name: "No_Nation" }, function (err, nation) {
+				if (err) return message.channel.send(errorMessage.setDescription('An error occurred.'));
+				if (!nation) return message.channel.send(errorMessage.setDescription('The database may be updating. Try again later.'));
 
-			let townList = [];
-			towns.forEach(town => {
-				townList.push(`${town.name.replace(/_/g, '\_')} - Members: ${town.residents} - Area: ${town.area}`);
+				let townList = [];
+				towns.forEach(town => {
+					townList.push(`${town.name.replace(/_/g, '\_')} - Members: ${town.residents} - Area: ${town.area}`);
+				})
+
+				let pages = townList.map(() => townList.splice(0, 10)).filter(a => a);
+				let embeds = [];
+
+				let pageNum = 0;
+				pages.forEach(page => {
+					pageNum++
+					let list = page.toString().replace(/,/g, '\n');
+					let embed = new Discord.MessageEmbed()
+						.setTitle('No Nation')
+						.addField('Towns', townList.length, true)
+						.addField('Residents', nation.residents, true)
+						.setDescription(`\`\`\`${list}\`\`\``)
+						.setColor(0x003175)
+						.setFooter(`Page ${pageNum}/${pages.length} | OneSearch`, 'https://cdn.bcow.tk/assets/logo-new.png');
+					embeds.push(embed);
+				})
+
+				message.channel.send(embeds[0]).then((m) => {
+					fn.paginator(message.author.id, m, embeds, 0);
+				});
+				message.channel.stopTyping();
 			})
-
-			let pages = townList.map(() => townList.splice(0, 10)).filter(a => a);
-			let embeds = [];
-
-			let pageNum = 0;
-			pages.forEach(page => {
-				pageNum++
-				let list = page.toString().replace(/,/g, '\n');
-				let embed = new Discord.MessageEmbed()
-					.setTitle('No Nation')
-					.setDescription(`\`\`\`${list}\`\`\``)
-					.setColor(0x003175)
-					.setFooter(`Page ${pageNum}/${pages.length} | OneSearch`, 'https://cdn.bcow.tk/assets/logo-new.png');
-				embeds.push(embed);
-			})
-
-			message.channel.send(embeds[0]).then((m) => {
-				fn.paginator(message.author.id, m, embeds, 0);
-			});
-			message.channel.stopTyping();
 		})
 	}
 };
