cb6ec5a504ceb7a4298685f18eef43428c434e96
imabritishcow <m@bcow.tk>
2020-08-20 22:44:47 -0400

    Added typing indications

--- commands/queue.js
+++ commands/queue.js
@@ -6,21 +6,26 @@ module.exports = {
   name: "queue",
   description: 'Shows current queue info',
   execute: async (message) => {
-    let server = await getPlayerCount('earthmc.net')
+    message.channel.startTyping();
+    let server = await getPlayerCount('earthmc.net');
 
     let mapData = await getMapData().catch(err => {
-      message.channel.send(errorMessage.setDescription('Error occurred while getting map data.'))
+      message.channel.stopTyping();
+      message.channel.send(errorMessage.setDescription('Error occurred while getting map data.'));
     });
     
     let queue = server.players.now - mapData.currentcount
     
-    let queueEmbed = new Discord.MessageEmbed().setColor(0x003175).setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png')
+    let queueEmbed = new Discord.MessageEmbed()
       .setTitle('Queue')
+      .setColor(0x003175)
       .setThumbnail('https://cdn.bcow.tk/logos/EarthMC.png')
       .addField('Server Total', `${server.players.now}/${server.players.max}`)
       .addField('In Queue', queue, true)
       .addField('Towny', `${mapData.currentcount >= 120 ? `**FULL** ${mapData.currentcount}` : mapData.currentcount}/120`, true)
+      .setFooter('OneSearch', 'https://cdn.bcow.tk/assets/logo-new.png');
     
-    message.channel.send(queueEmbed)
+    message.channel.send(queueEmbed);
+    message.channel.stopTyping();
   }
 }
