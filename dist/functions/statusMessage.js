b1bece2a1b36459b562dc7dc302f05e00e0fba31
No Name <imabritishcow@gmail.com>
2020-09-11 19:57:42 -0400

    Added dist to gitignore

--- dist/functions/statusMessage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.successMessage = exports.errorMessage = void 0;
-const Discord = require("discord.js");
-exports.errorMessage = new Discord.MessageEmbed().setTitle(':x: **Error**').setColor(0xdc2e44).setFooter('OneSearch', 'https://cdn.bcow.xyz/assets/onesearch.png');
-exports.successMessage = new Discord.MessageEmbed().setTitle(':white_check_mark: **Success!**').setColor(0x07bf63).setFooter('OneSearch', 'https://cdn.bcow.xyz/assets/onesearch.png');
-//# sourceMappingURL=statusMessage.js.map
