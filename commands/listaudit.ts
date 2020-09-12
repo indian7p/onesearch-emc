6107d16cf73bc769fa9938ea19373babb139db9a
No Name <imabritishcow@gmail.com>
2020-09-12 18:45:41 -0400

    Cleanup

--- commands/listaudit.ts
+++ commands/listaudit.ts
@@ -1,33 +1,29 @@
 import * as config from '../config.json';
 import { errorMessage } from '../functions/statusMessage';
+import { NationP, Nation } from '../models/models';
 
 export default {
 	name: 'listaudit',
 	description: 'Finds nations without statuses',
-	execute(message, Nation, NationP) {
+	execute: async (message) => {
 		if (!config.BOT_ADMINS.includes(message.author.id)) return message.channel.send(errorMessage.setDescription('You do not have permission to use this command.'));
 
-		Nation.find({}, function (err, nations) {
-			if (err) return message.channel.send(errorMessage.setDescription('An error occurred.'));
+		const nations = await Nation.find({}).exec().catch(err => message.channel.send(errorMessage.setDescription('An error occurred.')));
 
-			let counter = 0;
-			nations.forEach(nation => {
-				NationP.findOne({ name: nation.nameLower }, function (err, nationp) {
-					if (err) return message.channel.send(errorMessage.setDescription('An error occurred.'));
+		for (var i = 0; i < nations.length; i++) {
+			const nation = nations[i];
 
-					if (!nationp) {
-						message.channel.send(nation.name.replace(/_/g, '\_'));
-						counter++;
-					}else if(!nationp.status || nationp.status == ':grey_question: Unknown') {
-						message.channel.send(nation.name.replace(/_/g, '\_'));
-						counter++;
-					}
-				})
+			const nationp = await NationP.findOne({ name: nation.nameLower }).exec().catch(err => message.channel.send(errorMessage.setDescription('An error occurred.')));
 
-				if (counter == nations.length) {
-					message.channel.send(`Found ${counter} nations`);
-				}
-			})
-		});
+			if (!nationp) {
+				message.channel.send(nation.name.replace(/_/g, '\_'));
+			} else if (!nationp.status || nationp.status == ':grey_question: Unknown') {
+				message.channel.send(nation.name.replace(/_/g, '\_'));
+			}
+
+			if (i == nations.length - 1) {
+				message.channel.send(`Found ${i + 1} nations.`);
+			}
+		}
 	}
 };
