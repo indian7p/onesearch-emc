b8ba2c1ab9787272aa03e7217d23160ffb0bfd6a
No Name <imabritishcow@gmail.com>
2020-09-12 20:03:23 -0400

    Make embeds look nicer

--- commands/t/default.ts
+++ commands/t/default.ts
@@ -1,5 +1,4 @@
 import * as Discord from 'discord.js';
-import * as moment from 'moment-timezone';
 import { Town, TownP, Nation } from '../../models/models';
 import { errorMessage } from '../../functions/statusMessage';
 import calculateNationBonus from '../../functions/calculateNationBonus';
@@ -15,38 +14,35 @@ export default async (message, args) => {
   const townNationBonus = await calculateNationBonus(townNation);
 
   const townp = await TownP.findOne({ name: town.name }).exec().catch(err => message.channel.send(errorMessage.setDescription('An error occurred.')));
-
-  const description = !townp ? 'Information may be slightly out of date.' : townp.scrating == null ? 'Information may be slightly out of date.' : `**[Shootcity Rating: ${townp.scrating}]** Information may be slightly out of date.`;
   const imgLink = !townp ? 'https://cdn.bcow.xyz/assets/onesearch.png' : townp.imgLink == null ? 'https://cdn.bcow.xyz/assets/onesearch.png' : townp.imgLink;
   const link = !townp ? null : townp.link;
 
   const tName = town.capital == true ? `:star: ${town.name} (${town.nation})` : `${town.name} (${town.nation})`;
   const color = town.nation == 'No Nation' ? 0x69a841 : town.color == '#000000' ? 0x010101 : town.color == '#FFFFFF' ? 0xfefefe : town.color;
-  const timeUp = moment(town.time).tz('America/New_York').format('MMMM D, YYYY h:mm A z');
   const memberList = `\`\`\`${town.members}\`\`\``;
   const maxSize = town.membersArr.length * 8 > 800 ? 800 + townNationBonus : town.membersArr.length * 8 + townNationBonus;
 
   const resEmbed = new Discord.MessageEmbed()
     .setTitle(tName.replace(/_/g, '\_'))
-    .setDescription(description)
     .setColor(color)
     .setThumbnail(imgLink)
     .addField('Owner', `\`\`\`${town.mayor}\`\`\``, true)
     .addField('Location', `[${town.x}, ${town.z}](https://earthmc.net/map/?worldname=earth&mapname=flat&zoom=6&x=${town.x}&y=64&z=${town.z})`, true)
     .addField('Size', `${town.area}/${maxSize} [NationBonus: ${townNationBonus}]`, true)
-    .setFooter(`OneSearch | Last updated: ${timeUp}`, 'https://cdn.bcow.xyz/assets/onesearch.png');
+    .setTimestamp(town.time)
+    .setFooter(`OneSearch`, 'https://cdn.bcow.xyz/assets/onesearch.png');
 
   if (memberList.length > 1024) {
     let members1 = town.membersArr.slice(1, 50);
     let members2 = town.membersArr.slice(50, 100);
 
-    members1 = `\`\`\`${members1.toString().replace(/,/g, ', ')}\`\`\``;
-    members2 = `\`\`\`${members2.toString().replace(/,/g, ', ')}\`\`\``;
+    let members1str = `\`\`\`${members1.toString().replace(/,/g, ', ')}\`\`\``;
+    let members2str = `\`\`\`${members2.toString().replace(/,/g, ', ')}\`\`\``;
 
     if (!link) {
-      return resEmbed.addField(`Members [1-50]`, members1).addField(`Members [51-${town.membersArr.length}]`, members2);
+      return resEmbed.addField(`Members [1-50]`, members1str).addField(`Members [51-${town.membersArr.length}]`, members2str);
     } else {
-      return resEmbed.addField(`Members [1-50]`, members1).addField(`Members [51-${town.membersArr.length}]`, members2).setURL(link);
+      return resEmbed.addField(`Members [1-50]`, members1str).addField(`Members [51-${town.membersArr.length}]`, members2str).setURL(link);
     }
   } else {
     if (!link) {
